0eb8d71341ca2bcb3367c3f3d3897ad9b28affe7
qqniqq <69said05@gmail.com>
2025-10-31 20:59:06 +0300

    Add files via upload

---
+++ Hospital.Lib/IDrugRepository.cs
@@ -0,0 +1,12 @@
+﻿
+
+namespace Hospital.Lib
+{
+    public interface IDrugRepository
+    {
+        // Создать медикамент. Возвращает true при успехе, false при ошибке.
+        bool CreateDrug(Drug drug, out int createdId);
+
+        Drug GetDrugById(int id);
+    }
+}
